56bb9a5661f9118cbf25891a6e30cf6376d3d7a3
Marius Timmer <marius.timmer@uni-muenster.de>
2023-01-16 14:32:53 +0100

    Feature: Sitemap wird nun generiert

--- docs/scripts/providePostdata.js
+++ docs/scripts/providePostdata.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+var fqdn = "mariustimmer.de";
 var postProvider = require("../utilities/postProvider");
 var fs = require("fs");
 /**
@@ -6,9 +7,58 @@ var fs = require("fs");
  * so they can be used at runtime.
  */
 //console.log(postProvider.sidebarItems);
-fs.writeFile("./docs/postData.json", JSON.stringify(postProvider.sidebarItems), function (err) {
-    console.error(err);
+fs.writeFile("./docs/postData.json", JSON.stringify(postProvider.sidebarItems), console.error);
+fs.writeFile("./docs/tagData.json", JSON.stringify(postProvider.getTagSidebar()), console.error);
+// Sitemap
+var XMLWriter = require('xml-writer');
+var sitemap = new XMLWriter();
+sitemap.startDocument();
+sitemap.startElement("urlset");
+sitemap.writeAttribute("xmlns", "http://www.sitemaps.org/schemas/sitemap/0.9");
+var urls = [
+    {
+        link: "https://" + fqdn + "/",
+        lastmod: "2023-01-14",
+        changefreq: "never",
+        priority: "1.0"
+    },
+    {
+        link: "https://" + fqdn + "/post/",
+        lastmod: "2023-01-14",
+        changefreq: "daily",
+        priority: "0.9"
+    },
+    {
+        link: "https://" + fqdn + "/aboutme.html",
+        lastmod: "2023-01-14",
+        changefreq: "yearly",
+        priority: "0.5"
+    },
+    {
+        link: "https://" + fqdn + "/fotografie.html",
+        lastmod: "2022-12-01",
+        changefreq: "yearly",
+        priority: "0.5"
+    }
+];
+var articles = postProvider.sidebarItems.map(function (article) {
+    var dateObject = new Date(article.date);
+    return {
+        link: "https://" + fqdn + article.link + ".html",
+        lastmod: dateObject.toISOString().substring(0, 10),
+        changefreq: "weekly",
+        priority: "0.4"
+    };
 });
-fs.writeFile("./docs/tagData.json", JSON.stringify(postProvider.getTagSidebar()), function (err) {
-    console.error(err);
+[urls, articles].forEach(function (list) {
+    list.forEach(function (url) {
+        sitemap.startElement("url")
+            .writeElement("loc", url.link)
+            .writeElement("lastmod", url.lastmod)
+            .writeElement("changefreq", url.changefreq)
+            .writeElement("priority", url.priority)
+            .endElement();
+    });
 });
+sitemap.endDocument();
+fs.writeFile("./docs/sitemap.xml", sitemap.toString(), console.error);
--- docs/scripts/providePostdata.ts
+++ docs/scripts/providePostdata.ts
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+const fqdn = "mariustimmer.de";
 const postProvider = require("../utilities/postProvider");
 const fs = require("fs");
 
@@ -11,15 +12,72 @@ const fs = require("fs");
 fs.writeFile(
     "./docs/postData.json",
     JSON.stringify(postProvider.sidebarItems),
-    err => {
-        console.error(err)
-    }
+    console.error
 );
 
 fs.writeFile(
     "./docs/tagData.json",
     JSON.stringify(postProvider.getTagSidebar()),
-    err => {
-        console.error(err)
+    console.error
+);
+
+// Sitemap
+const XMLWriter = require('xml-writer');
+let sitemap = new XMLWriter();
+sitemap.startDocument();
+sitemap.startElement("urlset");
+sitemap.writeAttribute("xmlns", "http://www.sitemaps.org/schemas/sitemap/0.9");
+
+let urls = [
+    {
+        link: "https://"+ fqdn +"/",
+        lastmod: "2023-01-14",
+        changefreq: "never",
+        priority: "1.0"
+    },
+    {
+        link: "https://"+ fqdn +"/post/",
+        lastmod: "2023-01-14",
+        changefreq: "daily",
+        priority: "0.9"
+    },
+    {
+        link: "https://"+ fqdn +"/aboutme.html",
+        lastmod: "2023-01-14",
+        changefreq: "yearly",
+        priority: "0.5"
+    },
+    {
+        link: "https://"+ fqdn +"/fotografie.html",
+        lastmod: "2022-12-01",
+        changefreq: "yearly",
+        priority: "0.5"
     }
+];
+let articles = postProvider.sidebarItems.map((article) => {
+    let dateObject = new Date(article.date);
+    return {
+        link: "https://"+ fqdn + article.link +".html",
+        lastmod: dateObject.toISOString().substring(0, 10),
+        changefreq: "weekly",
+        priority: "0.4"
+    };
+});
+
+[urls, articles].forEach((list) => {
+    list.forEach((url) => {
+        sitemap.startElement("url")
+            .writeElement("loc", url.link)
+            .writeElement("lastmod", url.lastmod)
+            .writeElement("changefreq", url.changefreq)
+            .writeElement("priority", url.priority)
+            .endElement();
+    });
+});
+
+sitemap.endDocument();
+fs.writeFile(
+    "./docs/sitemap.xml",
+    sitemap.toString(),
+    console.error
 );
