772e6ff7ca85ba4a9af38bc7904840be5ce56640
Marius Timmer <marius.timmer@uni-muenster.de>
2025-07-02 12:46:05 +0200

    Improvement: Berechtigungen bei Put repariert

--- put.sh
+++ put.sh
@@ -4,12 +4,11 @@ HOST=nytlora
 DIRECTORY_SOURCE=./public/
 DIRECTORY_TARGET=/var/www/blog
 
+/usr/bin/ssh "${HOST}" /usr/bin/sudo /bin/chown -R timmer:timmer ${DIRECTORY_TARGET}/
 /usr/bin/rsync \
   -r \
   --delete \
   --update \
   "${DIRECTORY_SOURCE}" \
   "${HOST}":"${DIRECTORY_TARGET}" &&
-ssh \
-  "${HOST}" \
-  "/bin/chmod -R g+r ${DIRECTORY_TARGET}"
+/usr/bin/ssh "${HOST}" /usr/bin/sudo /bin/chown -R www-data:www-data ${DIRECTORY_TARGET}/
